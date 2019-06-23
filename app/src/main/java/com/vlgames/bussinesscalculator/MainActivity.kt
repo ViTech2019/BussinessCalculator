6ed3cd5a88bf9c953d3c44c34e2cc5c546a99b05
Your Name <you@example.com>
2019-06-23 21:39:22 +0300

    Initial commit

---
+++ app/src/main/java/com/vlgames/bussinesscalculator/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.vlgames.bussinesscalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
